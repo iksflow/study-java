5516ff769fa3a5b03d79099041e9b94601c27a7e
성승익 <seunglk.seong>
2019-11-05 13:09:10 +0900

    Learn IOStream basic#2...

--- src/training/iostream/OutputStreamExample.java
+++ src/training/iostream/OutputStreamExample.java
@@ -11,12 +11,18 @@ public class OutputStreamExample {
 		int count = 0;
 		OutputStream os = new FileOutputStream("C:/test2.txt");
 		byte[] data = "ABCD".getBytes();
+		
+		// 1byte씩 출력스트림으로 보낸다.
 //		for (int i = 0; i < data.length; i++) {
 //			os.write(data[i]);
 //			count++;
 //		}
 //		System.out.println("loop count : " + count);
 		
+		// byte배열에 저장된 모든 바이트를 내보낸다.
+//		os.write(data);
+		
+		// AB만 출력함.
 		os.write(data, 0, 2);
 		
 	}
