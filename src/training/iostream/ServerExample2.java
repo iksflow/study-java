88f9f49d271e4fbd9fc8b7b5ec33a0f497f4ac2e
성승익 <seunglk.seong>
2019-11-26 16:32:17 +0900

    Learn chat program.

--- src/training/iostream/ServerExample2.java
+++ src/training/iostream/ServerExample2.java
@@ -1,10 +1,17 @@
 package training.iostream;
 
+import java.net.ServerSocket;
+import java.util.List;
+import java.util.Vector;
+import java.util.concurrent.ExecutorService;
+
 public class ServerExample2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ExecutorService executorService;
+		ServerSocket serverSocket;
+		List<Client> connections = new Vector<Client>();
 	}
 
 }
