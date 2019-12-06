2053df5a1a788657632b942d872344f64843b6d2
성승익 <seunglk.seong>
2019-12-06 18:01:58 +0900

    Learn super constructor2

---
+++ src/training/inheritance/StudentExample.java
@@ -0,0 +1,14 @@
+package training.inheritance;
+
+public class StudentExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student student = new Student("ȫ�浿", "123456-1234456", 1);
+		
+		System.out.println("name : " + student.name);
+		System.out.println("ssn : " + student.ssn);
+		System.out.println("studentNo : " + student.studentNo);
+	}
+
+}
